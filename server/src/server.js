9caac00e0da77f0f677b6bb74242c36a8f6bcb98
Charles <charlesmbrady@gmail.com>
2019-09-21 16:01:07 -0400

    try removing /static from end up build path line 15

--- server/src/server.js
+++ server/src/server.js
@@ -12,7 +12,7 @@ app.use(express.json({ limit: '100mb' }));
 // If its production environment!
 if (process.env.NODE_ENV === 'production') {
 	const path = require('path');
-	app.use('/static', express.static(path.join(__dirname, '../../client/build/static')));
+	app.use('/static', express.static(path.join(__dirname, '../../client/build/')));
 	app.get('/', (req, res) => {
 		res.sendFile(path.join(__dirname, '../../client/build/'));
 	});
