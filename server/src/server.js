adc74bb8e43eb3a5ae54449b0690eca2ffb7b7ba
Charles <charlesmbrady@gmail.com>
2019-09-22 01:11:57 -0400

    required routes

--- server/src/server.js
+++ server/src/server.js
@@ -3,21 +3,24 @@ const express = require('express');     // Require express
 const app = express();                  // Make app with express
 const PORT = process.env.PORT || 3001;  // Set port to .env or default
 const dbConnection = require('./db/index.js');  // Require the database connection for server to access
-const path = require('path');
+const routes = require('./routes');    // Require the routes to use for api endpoints
+const path = require('path');           // Lets us use __dirname as the relative filepath from this file
 
 // Middlewares
 app.use(express.urlencoded({ limit: '100mb', extended: true }));
 app.use(express.json({ limit: '100mb' }));
 
-// If its production environment!
+// If its production environment...
 if (process.env.NODE_ENV === 'production') {
-	app.use(express.static(path.join(__dirname, '../../client/build')));
+	app.use(express.static(path.join(__dirname, '../../client/build')));        // Specify the location of static files
 	app.get('/', function (req, res) {
-        res.sendFile(path.join(__dirname, '../../client/build', 'index.html'));
+        res.sendFile(path.join(__dirname, '../../client/build', 'index.html')); // Return our index.html page when home "/" route is hit
       });
 }
 
 app.get('/test', (req, res) => res.send('Hello world!'));   // Basic response for testing server
+
+app.use(routes);    // Link routes
 
 // Error handler
 app.use(function (err, req, res, next) {
